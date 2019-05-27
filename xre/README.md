177f4caefdb38179559e0b01ad5c5c743be04685
zoujiangtao <zoujiangtao@360.cn>
2019-05-27 15:58:22 +0800

    create xre.py

---
+++ xre/README.md
@@ -0,0 +1,41 @@
+### Usage
+
+批量查看多台机器是否ping通，并把成功的机器和失败的机器分别各自文件中
+
+> eg:查看 openstack01.zoushiwen.com 到 openstack10.zoushiwen.com 能否ping通
+~~~
+zoujiangtao:~$ xre.py openstack[1-10].zoushiwen.com
+openstack01.zoushiwen.com DOWN
+openstack02.zoushiwen.com DOWN
+openstack03.zoushiwen.com DOWN
+openstack04.zoushiwen.com DOWN
+openstack05.zoushiwen.com DOWN
+openstack06.zoushiwen.com DOWN
+openstack07.zoushiwen.com DOWN
+openstack08.zoushiwen.com DOWN
+openstack09.zoushiwen.com DOWN
+
+DOWN: ping失败
+UP: ping成功
+~~~
+
+
+#### Result
+
+~~~
+在根目录生成success_file和 fail_file两个文件
+
+cat ~/success_hosts
+
+zoujiangtao:~$ cat ~/fail_hosts
+openstack01.zoushiwen.com
+openstack02.zoushiwen.com
+openstack03.zoushiwen.com
+openstack04.zoushiwen.com
+openstack05.zoushiwen.com
+openstack06.zoushiwen.com
+openstack07.zoushiwen.com
+openstack08.zoushiwen.com
+openstack09.zoushiwen.com
+
+~~~
---
+++ xre/xre.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+import sys
+import subprocess
+import re
+import os.path
+
+def getIndex(host):
+    res = host.split('.')
+    realHost = list()
+    first, end = None, None
+    for i in res:
+        if '-' in i:
+            j = i.split('-')
+            for m in j:
+                if '[' in m:
+                    first = int(m.split('[')[1])
+                elif ']' in m:
+                    end = int(m.split(']')[0])
+                else:
+                    pass
+            if end < first:
+                print("The index value must {} greater than {}.".format(end,first))
+                sys.exit(1)
+
+            res1 = host.split('[')[0]
+            res2 = host.split(']')[1]
+            for i in range(first, end):
+                if i < 10:
+                    resHost = res1 + '0' + str(i) + res2
+                else:
+                    resHost = res1 + str(i) + res2
+                realHost.append(resHost)
+            return realHost
+
+def check_alive(ip_list, count=1, timeout=1):
+    suceessFile = os.path.join(os.path.expanduser('~'), 'success_hosts')
+    failFile = os.path.join(os.path.expanduser('~'), 'fail_hosts')
+
+    success_ip,fail_ip = list(),list()
+    for ip in ip_list:
+
+        cmd = 'ping -c %d -t %d  %s' % (count, timeout, ip)
+
+        p = subprocess.Popen(cmd,
+                             stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE,
+                             shell=True
+                             )
+
+        result = p.stdout.read()
+        regex = re.findall('0.0% packet loss', result)
+        if len(regex) != 0:
+            print "\033[32m%s UP\033[0m" % (ip)
+            success_ip.append(ip + '\n')
+        else:
+            print "\033[31m%s DOWN\033[0m" % (ip)
+            fail_ip.append(ip + '\n')
+
+    fileOption(suceessFile,success_ip)
+    fileOption(failFile,fail_ip)
+
+
+def fileOption(file,contents):
+    with open(file,'w+') as f:
+        for content in contents:
+            f.writelines(content)
+
+if __name__ == '__main__':
+
+    try:
+        host = sys.argv[1]
+    except IndexError as e:
+        print e.message
+        sys.exit(1)
+
+    realHost = getIndex(host)
+    check_alive(realHost)
+
+
+
+
+
+
+
